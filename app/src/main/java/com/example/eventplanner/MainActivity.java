2ca50826e5cb9e46753676b1d95c590fccc607c3
ISIRA_PC <it19017334@my.sliit.lk>
2020-08-16 14:17:59 +0530

    Welcome Page 2nd Commit

--- app/src/main/java/com/example/eventplanner/MainActivity.java
+++ app/src/main/java/com/example/eventplanner/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.eventplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,Home.class);
+
+                Context context = getApplicationContext();
+                CharSequence text = context.getString(R.string.welcome);
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 10);
+                toast.show();
+
                 startActivity(intent);
             }
         });
