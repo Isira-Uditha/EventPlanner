6910ba58782561c3583d9ed67fb6e0d5943aec39
THARUSHIKA <it19016108@my.sliit.lk>
2020-09-21 22:43:08 +0530

    Update DBHelper.

--- app/src/main/java/com/example/eventplanner/Database/DBHelper.java
+++ app/src/main/java/com/example/eventplanner/Database/DBHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import com.example.eventplanner.Event;
+import com.example.eventplanner.Database.Event;
 
 import java.util.ArrayList;
 import java.util.List;
